95828214b74c627829198251ec50cd7f4426d768
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-06-28 17:00:47 +0800

    updata

--- php/balogin.php
+++ php/balogin.php
@@ -1,3 +1,22 @@
 <?php
+if (isset($_POST) && !empty($_POST)){
+    $error = array();
+    // $account = FN::ch_email($_POST['account']);
+    // $password = FN::ch_value($_POST['password']);
+
+
+    //帳號
+    if($account['re']==false){
+        array_push($error,$account['me']);
+    }else{
+        $account = $account['me'];
+    }
+
+
+    //密碼
+    if (empty($password)) {
+        array_push($error,"密碼未填");
+    }
+}
 include('page/component/balogin.php');
 
