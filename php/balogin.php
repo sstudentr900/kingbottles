15b79bdeffc24738e4b35a911717d5fad709bf7c
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-06-28 17:07:22 +0800

    updata

--- php/balogin.php
+++ php/balogin.php
@@ -17,6 +17,28 @@ if (isset($_POST) && !empty($_POST)){
     if (empty($password)) {
         array_push($error,"密碼未填");
     }
+
+    //驗證帳號密碼
+    if(empty($error)){
+        $dataName='manager';
+        $condition = "Password=:Password AND Account=:Account AND is_Release = 'Y'";
+        $order_by = "";
+        $fields = "id,Name";
+        $limit='';
+        $passwrd = md5('@#mj'.$password.'app!');
+        $data_array = array('Account'=>$account,'Password'=>$passwrd);
+        $manager = Database::get()->query2($dataName, $condition, $order_by, $fields, $limit, $data_array);
+        $row = Database::get()->getTotle();
+
+        if($row){
+            $_SESSION['baMemberID']= $manager[0]['id'];
+            header("Location: ?a=bamanager");
+            exit();
+        }else{
+            array_push($error,"帳號或密碼錯誤");
+        }
+    }
+    $error = implode(',',$error);
 }
 include('page/component/balogin.php');
 
